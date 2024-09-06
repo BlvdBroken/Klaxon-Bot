df82fc83d3e8aee2efc3460748e9bd245246c90f
BlvdBroken <skazimirowicz@gmail.com>
2024-09-06 00:24:31 -0700

    1.1 add k!ignore command
    ignores given channels, now logs klaxons triggered

--- klxn.py
+++ klxn.py
@@ -10,6 +10,7 @@ class MyClient(discord.Client):
 
     word = "test"
     usr = None
+    ignored = []
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
@@ -20,6 +21,20 @@ class MyClient(discord.Client):
         # does nothing if bots say it
         if message.author == self.user:
             return
+        
+        # ignore channels
+        if message.content.startswith("k!ignore"):
+            if message.channel.id in self.ignored:
+                self.ignored.remove(message.channel.id)
+                await message.channel.send("<#{0}> unignored.".format(message.channel.id))
+            else:
+                self.ignored.append(message.channel.id)
+                await message.channel.send("<#{0}> ignored.".format(message.channel.id))
+            return
+        
+        # do not listen to ignored channels (except ignore command)
+        if message.channel.id in self.ignored:
+            return
 
         # klaxon word finder
         if self.word and (self.word in message.content.lower()):
@@ -27,6 +42,7 @@ class MyClient(discord.Client):
             await message.channel.send("# :camera_with_flash: The Klaxon word \'{0}\' was said by <@{1}> and earned them -10 points! :camera_with_flash:".format(self.word, message.author.id))
             self.word = None
             await message.author.send("Please respond with a new Klaxon word. Choose wisely.")
+            print("{0} said by {1} in {2}".format(message.content, message.author.name, message.channel.name))
             return
     
         # user dm understander
@@ -38,6 +54,7 @@ class MyClient(discord.Client):
                 self.usr = None
             else:
                 await message.author.send("I said word. \'{0}\' isn't a word. Idiot.".format(tempword))
+            return
 
 intents = discord.Intents.default()
 intents.members = True
